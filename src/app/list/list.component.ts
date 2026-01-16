33fb6ee4678cb97b140e002f9b3e0f2001fde239
AntonioLabellarteSC <a.labellarte@studiocicchelli.net>
2026-01-16 13:08:26 +0100

    Push generico

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
-export class ListComponent {
 
+// status: completato - incorso - annullato
+
+export class ListComponent {
+  tasks = [
+    {
+      id: '1',
+      title: 'Obiettivo',
+      description: 'Obiettivo da completare',
+      status: 'incorso',
+    },
+  ]
 }
