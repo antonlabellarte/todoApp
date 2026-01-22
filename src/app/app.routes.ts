4ad0b45a408aa1bb46b9b0a679175e6ea7b8e653
AntonioLabellarteSC <a.labellarte@studiocicchelli.net>
2026-01-22 13:09:41 +0100

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
+import { AddtaskComponent } from './addtask/addtask.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
-    { path: 'list', component: ListComponent}
+    { path: 'list', component: ListComponent},
+    { path: 'addtask', component: AddtaskComponent }
 ];
